80817014575141b69afb38b84a92e8fc46b53125
leiber bertel <leiberbertel777@gmail.com>
2023-11-01 21:51:55 -0500

    refactor: improve crud methods

--- src/main/java/com/senaanalisis/TodoApp/service/TaskService.java
+++ src/main/java/com/senaanalisis/TodoApp/service/TaskService.java
@@ -1,8 +1,11 @@
 package com.senaanalisis.TodoApp.service;
 
+import com.senaanalisis.TodoApp.persistence.entity.TaskEntity;
 import com.senaanalisis.TodoApp.persistence.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TaskService {
@@ -13,4 +16,37 @@ public class TaskService {
     public TaskService(TaskRepository taskRepository) {
         this.taskRepository = taskRepository;
     }
+
+
+    public TaskEntity getTask(int id) {
+        return this.taskRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Task not found"));
+    }
+
+    public TaskEntity create(TaskEntity task) {
+        if(task.getTaskId() == null) {
+            return this.taskRepository.save(task);
+        }
+        throw new RuntimeException("Task already exists");
+    }
+
+    public TaskEntity update(TaskEntity task) {
+        if(task.getTaskId() != null) {
+            return this.taskRepository.save(task);
+        } else {
+            throw new RuntimeException("Task not found");
+        }
+    }
+
+    public void delete(int id) {
+        if(!this.taskRepository.existsById(id)) {
+            throw new RuntimeException("Task not found");
+        }
+        this.taskRepository.deleteById(id);
+    }
+
+    public List<TaskEntity> getAll() {
+        return this.taskRepository.findAll();
+    }
+
 }
--- src/main/java/com/senaanalisis/TodoApp/service/UserService.java
+++ src/main/java/com/senaanalisis/TodoApp/service/UserService.java
@@ -1,18 +1,63 @@
 package com.senaanalisis.TodoApp.service;
 
+import com.senaanalisis.TodoApp.exception.UserAlreadyExistsException;
+import com.senaanalisis.TodoApp.exception.UserNotFoundException;
+import com.senaanalisis.TodoApp.persistence.entity.UserEntity;
 import com.senaanalisis.TodoApp.persistence.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
+    public UserEntity getUser(int id) {
+        return this.userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException("User with id " + id + " not found"));
+    }
+
+    @Transactional
+    public UserEntity create(UserEntity user) {
+        if (user.getId() != null) {
+            throw new UserAlreadyExistsException("User with ID " + user.getId() + " already exists");
+        }
+
+        Optional<UserEntity> existingUser = userRepository.findByEmail(user.getEmail());
+        if (existingUser.isPresent()) {
+            throw new UserAlreadyExistsException("User with email " + user.getEmail() + " already exists");
+        }
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        return userRepository.save(user);
+    }
+
+    @Transactional
+    public UserEntity update(UserEntity user) {
+        if (user.getId() == null || !this.userRepository.existsById(user.getId())) {
+            throw new UserNotFoundException("User not found");
+        }
+        return this.userRepository.save(user);
+    }
+
+    @Transactional
+    public void delete(int id) {
+        if (!this.userRepository.existsById(id)) {
+            throw new UserNotFoundException("User with id " + id + " not found");
+        }
+        this.userRepository.deleteById(id);
+    }
 
 }
