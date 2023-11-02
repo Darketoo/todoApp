27f4996cf702b40727dd7a7756c9ec02f79dcb33
leiber bertel <leiberbertel777@gmail.com>
2023-11-01 21:54:41 -0500

    feat: add method findByEmail for entity User

--- src/main/java/com/senaanalisis/TodoApp/persistence/repository/UserRepository.java
+++ src/main/java/com/senaanalisis/TodoApp/persistence/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.senaanalisis.TodoApp.persistence.repository;
 import com.senaanalisis.TodoApp.persistence.entity.UserEntity;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends ListCrudRepository<UserEntity, Integer> {
+    Optional<UserEntity> findByEmail(String email);
 }
