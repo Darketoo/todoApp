58785a6c18c023bd8ba907479b7708a7a2e6e2c1
leiber bertel <leiberbertel777@gmail.com>
2023-11-06 13:05:39 -0500

    perf: update repositories add new methods

--- src/main/java/com/senaanalisis/TodoApp/persistence/repository/TaskRepository.java
+++ src/main/java/com/senaanalisis/TodoApp/persistence/repository/TaskRepository.java
@@ -3,5 +3,8 @@ package com.senaanalisis.TodoApp.persistence.repository;
 import com.senaanalisis.TodoApp.persistence.entity.TaskEntity;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.util.List;
+
 public interface TaskRepository extends ListCrudRepository<TaskEntity, Integer> {
+    List<TaskEntity> findByUserId(Integer user_id);
 }
--- src/main/java/com/senaanalisis/TodoApp/persistence/repository/UserRepository.java
+++ src/main/java/com/senaanalisis/TodoApp/persistence/repository/UserRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.repository.ListCrudRepository;
 import java.util.Optional;
 
 public interface UserRepository extends ListCrudRepository<UserEntity, Integer> {
-    Optional<UserEntity> findByEmail(String email);
+    Optional<UserEntity> findByUsername(String username);
 }
