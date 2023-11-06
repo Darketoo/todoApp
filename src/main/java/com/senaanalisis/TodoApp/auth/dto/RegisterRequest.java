54ef1fe645af99138583bf12ed5ee3637c59b73d
leiber bertel <leiberbertel777@gmail.com>
2023-11-06 13:03:48 -0500

    perf: add packages for security of API

---
+++ src/main/java/com/senaanalisis/TodoApp/auth/dto/RegisterRequest.java
@@ -0,0 +1,19 @@
+package com.senaanalisis.TodoApp.auth.dto;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    private String username;
+    private String password;
+    private String name;
+    @Column(nullable = true)
+    private String email;
+}
