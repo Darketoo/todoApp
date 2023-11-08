955e97716f2ace227d862b57ae62c6c0455a5075
leiber bertel <leiberbertel777@gmail.com>
2023-11-07 23:03:44 -0500

    build: add docs

--- src/main/java/com/senaanalisis/TodoApp/auth/dto/AuthResponse.java
+++ src/main/java/com/senaanalisis/TodoApp/auth/dto/AuthResponse.java
@@ -1,6 +1,7 @@
 package com.senaanalisis.TodoApp.auth.dto;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,5 +12,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AuthResponse {
+
+    @Schema(description = "token of user", example = "38623786432he2uyhdg623gt7t236tr72g")
     String token;
 }
