384893ae33e6e555d28f8b9a1d9a563f4c710040
leiber bertel <leiberbertel777@gmail.com>
2023-11-07 20:49:26 -0500

    build: add class for docs with Spring doc (open api)

---
+++ src/main/java/com/senaanalisis/TodoApp/web/config/OpenApiConfig.java
@@ -0,0 +1,22 @@
+package com.senaanalisis.TodoApp.web.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API To-do")
+                        .version("1.0")
+                        .description("API para el funcionamiento básico de una aplicación To-do con Spring Security")
+                        .contact(new Contact()
+                                .name("Sena-Análisis")));
+    }
+}
