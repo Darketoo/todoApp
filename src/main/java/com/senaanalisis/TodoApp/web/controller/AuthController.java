84239083fa225509e68ea6c2016ed7b4c391eb3d
leiber bertel <leiberbertel777@gmail.com>
2023-11-07 21:27:49 -0500

    refactor: add docs of controller

--- src/main/java/com/senaanalisis/TodoApp/web/controller/AuthController.java
+++ src/main/java/com/senaanalisis/TodoApp/web/controller/AuthController.java
@@ -4,6 +4,9 @@ import com.senaanalisis.TodoApp.auth.dto.AuthResponse;
 import com.senaanalisis.TodoApp.auth.dto.LoginRequest;
 import com.senaanalisis.TodoApp.auth.dto.RegisterRequest;
 import com.senaanalisis.TodoApp.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,11 +21,21 @@ public class AuthController {
 
     private final AuthService authService;
 
+    @Operation(summary = "Income a Application")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "403", description = "Deny access")
+    })
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
         return ResponseEntity.ok(authService.login(request));
     }
 
+    @Operation(summary = "Register a user a Application")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "403", description = "Deny access")
+    })
     @PostMapping("/register")
     public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request) {
         return ResponseEntity.ok(authService.register(request));
